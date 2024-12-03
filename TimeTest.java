18225da28d0faa34d10e1759859a4b6b9a237fdf
OtavioFarias <137441117+OtavioFarias@users.noreply.github.com>
2024-12-02 23:57:21 -0300

    Update TimeTest.java

--- TimeTest.java
+++ TimeTest.java
@@ -18,70 +18,70 @@ public class TimeTest {
         List<Long> Dijkstral_Matriz = new ArrayList<>();
 
       Graph graph, graphM;
-      graph = new Graph(new In(new File("sample100-1980.gr")), false);
-      graphM = new Graph(new In(new File("sample100-1980.gr")), true);
+      graph = new Graph(new In(new File("Grafos de Entrada/sample100-1980.gr")), false);
+      graphM = new Graph(new In(new File("Grafos de Entrada/sample100-1980.gr")), true);
 
       switch(Integer.parseInt(args[0])){
 
         case 1:
-            graph = new Graph(new In(new File("sample100-1980.gr")), false);
-            graphM = new Graph(new In(new File("sample100-1980.gr")), true);
+            graph = new Graph(new In(new File("Grafos de Entrada/sample100-1980.gr")), false);
+            graphM = new Graph(new In(new File("Grafos de Entrada/sample100-1980.gr")), true);
         break;
         case 2:
-            graph = new Graph(new In(new File("sample100-3960.gr")), false);
-            graphM = new Graph(new In(new File("sample100-3960.gr")), true);
+            graph = new Graph(new In(new File("Grafos de Entrada/sample100-3960.gr")), false);
+            graphM = new Graph(new In(new File("Grafos de Entrada/sample100-3960.gr")), true);
         break;
         case 3:
-            graph = new Graph(new In(new File("sample100-5940.gr")), false);
-            graphM = new Graph(new In(new File("sample100-5940.gr")), true);
+            graph = new Graph(new In(new File("Grafos de Entrada/sample100-5940.gr")), false);
+            graphM = new Graph(new In(new File("Grafos de Entrada/sample100-5940.gr")), true);
         break;
         case 4:
-            graph = new Graph(new In(new File("sample100-7920.gr")), false);
-            graphM = new Graph(new In(new File("sample100-7920.gr")), true);
+            graph = new Graph(new In(new File("Grafos de Entrada/sample100-7920.gr")), false);
+            graphM = new Graph(new In(new File("Grafos de Entrada/sample100-7920.gr")), true);
         break;
         case 5:
-            graph = new Graph(new In(new File("sample100-9900.gr")), false);
-            graphM = new Graph(new In(new File("sample100-9900.gr")), true);
+            graph = new Graph(new In(new File("Grafos de Entrada/sample100-9900.gr")), false);
+            graphM = new Graph(new In(new File("Grafos de Entrada/sample100-9900.gr")), true);
         break;
         case 6:
-            graph = new Graph(new In(new File("sample200-7960.gr")), false);
-            graphM = new Graph(new In(new File("sample200-7960.gr")), true);
+            graph = new Graph(new In(new File("Grafos de Entrada/sample200-7960.gr")), false);
+            graphM = new Graph(new In(new File("Grafos de Entrada/sample200-7960.gr")), true);
         break;
         case 7:
-            graph = new Graph(new In(new File("sample200-15920.gr")), false);
-            graphM = new Graph(new In(new File("sample200-15920.gr")), true);
+            graph = new Graph(new In(new File("Grafos de Entrada/sample200-15920.gr")), false);
+            graphM = new Graph(new In(new File("Grafos de Entrada/sample200-15920.gr")), true);
         break;
         case 8:
-            graph = new Graph(new In(new File("sample200-23880.gr")), false);
-            graphM = new Graph(new In(new File("sample200-23880.gr")), true);
+            graph = new Graph(new In(new File("Grafos de Entrada/sample200-23880.gr")), false);
+            graphM = new Graph(new In(new File("Grafos de Entrada/sample200-23880.gr")), true);
         break;
         case 9:
-            graph = new Graph(new In(new File("sample200-31840.gr")), false);
-            graphM = new Graph(new In(new File("sample200-31840.gr")), true);
+            graph = new Graph(new In(new File("Grafos de Entrada/sample200-31840.gr")), false);
+            graphM = new Graph(new In(new File("Grafos de Entrada/sample200-31840.gr")), true);
         break;
         case 10:
-            graph = new Graph(new In(new File("sample200-39800.gr")), false);
-            graphM = new Graph(new In(new File("sample200-39800.gr")), true);
+            graph = new Graph(new In(new File("Grafos de Entrada/sample200-39800.gr")), false);
+            graphM = new Graph(new In(new File("Grafos de Entrada/sample200-39800.gr")), true);
         break;
         case 11:
-            graph = new Graph(new In(new File("sample500-49900.gr")), false);
-            graphM = new Graph(new In(new File("sample500-49900.gr")), true);
+            graph = new Graph(new In(new File("Grafos de Entrada/sample500-49900.gr")), false);
+            graphM = new Graph(new In(new File("Grafos de Entrada/sample500-49900.gr")), true);
         break;
         case 12:
-            graph = new Graph(new In(new File("sample500-99800.gr")), false);
-            graphM = new Graph(new In(new File("sample500-99800.gr")), true);
+            graph = new Graph(new In(new File("Grafos de Entrada/sample500-99800.gr")), false);
+            graphM = new Graph(new In(new File("Grafos de Entrada/sample500-99800.gr")), true);
         break;
         case 13:
-            graph = new Graph(new In(new File("sample500-149700.gr")), false);
-            graphM = new Graph(new In(new File("sample500-149700.gr")), true);
+            graph = new Graph(new In(new File("Grafos de Entrada/sample500-149700.gr")), false);
+            graphM = new Graph(new In(new File("Grafos de Entrada/sample500-149700.gr")), true);
         break;
         case 14:
-            graph = new Graph(new In(new File("sample500-199600.gr")), false);
-            graphM = new Graph(new In(new File("sample500-199600.gr")), true);
+            graph = new Graph(new In(new File("Grafos de Entrada/sample500-199600.gr")), false);
+            graphM = new Graph(new In(new File("Grafos de Entrada/sample500-199600.gr")), true);
         break;
         case 15:
-            graph = new Graph(new In(new File("sample500-249500.gr")), false);
-            graphM = new Graph(new In(new File("sample500-249500.gr")), true);
+            graph = new Graph(new In(new File("Grafos de Entrada/sample500-249500.gr")), false);
+            graphM = new Graph(new In(new File("Grafos de Entrada/sample500-249500.gr")), true);
         break;
 	}
 
@@ -111,7 +111,7 @@ public class TimeTest {
 
             // Dijkstral_Lista
             t_ini = System.nanoTime();
-            graph.Dijkstral_Lista();
+            graph.Dijkstra_Lista(0);
             t_fim = System.nanoTime();
             Dijkstral_Lista.add(t_fim - t_ini);
             
@@ -119,7 +119,7 @@ public class TimeTest {
             
             // Dijkstral_Matriz
             t_ini = System.nanoTime();
-            graphM.Dijkstral_Matriz();
+            graphM.Dijkstra_Matriz(0);
             t_fim = System.nanoTime();
             Dijkstral_Matriz.add(t_fim - t_ini);
 
@@ -128,7 +128,7 @@ public class TimeTest {
 
     }
 
-        String nomeArquivoCSV = "TemposExecucao" + args[0] + "testes" + "grafo" + args[1] + ".csv";
+        String nomeArquivoCSV = "arq" + args[0] + ".csv";
 
         
         try (PrintWriter writer = new PrintWriter(new File(nomeArquivoCSV))) {
@@ -148,11 +148,11 @@ public class TimeTest {
             for (Long tempo : Bellman_Ford_Matriz) writer.print("," + tempo);
             writer.println();
 
-            writer.print("Dijkstral Lista");
+            writer.print("Dijkstra Lista");
             for (Long tempo : Dijkstral_Lista) writer.print("," + tempo);
             writer.println();
 
-            writer.print("Dijkstral_Matriz");
+            writer.print("Dijkstra Matriz");
             for (Long tempo : Dijkstral_Matriz) writer.print("," + tempo);
             writer.println();
 
